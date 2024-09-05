5d0ed10934005b0d90d9322733004a20ab348dbd
Ayomide2007 <ayomidepaul784@gmail.com>
2024-09-05 01:20:20 +0100

    second-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleMenu = () => {
